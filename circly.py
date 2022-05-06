6d14d7c6deec7a40042c22c4ce7973906a13103a
Jesse Kretschmer <jesse@krets.com>
2022-05-06 11:57:09 +0200

    Insta crop and color swap

--- circly.py
+++ circly.py
@@ -6,7 +6,7 @@ import math
 
 
 def size_mult(res, mult):
-    return res[0] * mult, res[1] * mult
+    return int(res[0] * mult), int(res[1] * mult)
 
 
 def circle(center, radius):
@@ -30,12 +30,14 @@ def max_radius(line, offset):
     return abs(offset - c)
 
 
-def circly(center, draw, radius, count=90, move=.06, padding=15, outline='black', width=2):
+def circly(center, draw, radius, count=999, move=.06, padding=15, outline='black', width=2):
     for i in range(count):
         draw.ellipse(circle(center, radius), outline=outline, width=width)
         new_center = new_point(center, int(radius * move))
         new_radius = max_radius(list(center) + list(new_center), radius - padding)
-
+        if new_radius < width:
+            print("Stop iteration due to small circle: %i" % i)
+            break
         center, radius = new_center, new_radius
 
 
@@ -50,8 +52,12 @@ def draw(size, cols, rows, resample_scale=8, bg='white', outline='black'):
             center = (img.width / cols * (x), img.height / rows * (y))
             radius = min(img.width / cols, img.height / rows) * .5 * (random.random() * .4 + .6)
             circly(center, draw, radius, outline=outline, width=2*resample_scale)
-
-    img.resize(size).show()
+    return img.resize(size)
 
 if __name__ == '__main__':
-    draw((4096, 2048), 14, 7)
+    insta_max = (1080, 1350)
+    size = size_mult(insta_max, 1.2)
+    img = draw(size, 10, 13, bg=(50, 50, 50), outline='white')
+    crop_pad = [(img.width - 1080)//2, (img.height - 1350)//2]
+    crop_box = crop_pad + [img.width - crop_pad[0], img.height - crop_pad[1]]
+    img.crop(crop_box).show()
