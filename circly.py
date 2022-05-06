1443e37fcd7f4973147bdbc0f8fec080b37f360f
Jesse Kretschmer <jesse@krets.com>
2022-05-06 13:36:03 +0200

    noisy background

--- circly.py
+++ circly.py
@@ -4,6 +4,8 @@ from PIL import Image, ImageDraw
 import random
 import math
 
+from perlin_noise import PerlinNoise
+
 
 def size_mult(res, mult):
     return int(res[0] * mult), int(res[1] * mult)
@@ -43,8 +45,30 @@ def circly(center, draw, radius, count=999, move=.06, padding=15, outline='black
         center, radius = new_center, new_radius
 
 
+def interpolate(color_a, color_b, mult):
+    ra, ga, ba = color_a
+    rb, gb, bb = color_b
+
+    rc = int(abs(ra - rb) * mult + min(ra, rb))
+    gc = int(abs(ga - gb) * mult + min(ga, gb))
+    bc = int(abs(ba - bb) * mult + min(ba, bb))
+
+    return rc, gc, bc
+
+
 def draw(size, cols, rows, resample_scale=8, bg='white', outline='black'):
     img = Image.new('RGBA', size_mult(size, resample_scale), bg)
+
+    background = Image.new('RGBA', size_mult(size, .25))
+    noise = PerlinNoise(octaves=10, seed=1)
+    color_a = (0, 31, 84)
+    color_b = (5, 44, 101)
+    for x in range(background.width):
+        for y in range(background.height):
+            val = noise([x/background.width, y/background.height])
+            background.putpixel((x, y), interpolate(color_a, color_b, val))
+    img.paste(background.resize(img.size, resample=Image.BICUBIC))
+
     draw = ImageDraw.Draw(img)
     rows += 1
     cols += 1
@@ -59,7 +83,7 @@ def draw(size, cols, rows, resample_scale=8, bg='white', outline='black'):
 if __name__ == '__main__':
     insta_max = (1080, 1350)
     size = size_mult(insta_max, 1.3)
-    img = draw(size, 8, 10, bg='#011e56', outline=['#306596', '#88aec6', '#e6f2f2'])
+    img = draw(size, 8, 10, bg=(1, 35, 91), outline=['#306596', '#88aec6', '#e6f2f2'])
     crop_pad = [(img.width - 1080)//2, (img.height - 1350)//2]
     crop_box = crop_pad + [img.width - crop_pad[0], img.height - crop_pad[1]]
     img.crop(crop_box).show()
