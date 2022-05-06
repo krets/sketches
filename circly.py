25150d1fa8aba2dddaa8be0dc866308da6bfa4ba
Jesse Kretschmer <jesse@krets.com>
2022-05-06 12:58:51 +0200

    cyano colors

--- circly.py
+++ circly.py
@@ -32,7 +32,9 @@ def max_radius(line, offset):
 
 def circly(center, draw, radius, count=999, move=.06, padding=15, outline='black', width=2):
     for i in range(count):
-        draw.ellipse(circle(center, radius), outline=outline, width=width)
+        if isinstance(outline, list):
+            color = random.choice(outline)
+        draw.ellipse(circle(center, radius), outline=color, width=width)
         new_center = new_point(center, int(radius * move))
         new_radius = max_radius(list(center) + list(new_center), radius - padding)
         if new_radius < width:
@@ -51,13 +53,13 @@ def draw(size, cols, rows, resample_scale=8, bg='white', outline='black'):
         for y in range(1, rows):
             center = (img.width / cols * (x), img.height / rows * (y))
             radius = min(img.width / cols, img.height / rows) * .5 * (random.random() * .4 + .6)
-            circly(center, draw, radius, outline=outline, width=2*resample_scale)
+            circly(center, draw, radius, outline=outline, width=int(1.3*resample_scale))
     return img.resize(size)
 
 if __name__ == '__main__':
     insta_max = (1080, 1350)
-    size = size_mult(insta_max, 1.2)
-    img = draw(size, 10, 13, bg=(50, 50, 50), outline='white')
+    size = size_mult(insta_max, 1.3)
+    img = draw(size, 8, 10, bg='#011e56', outline=['#306596', '#88aec6', '#e6f2f2'])
     crop_pad = [(img.width - 1080)//2, (img.height - 1350)//2]
     crop_box = crop_pad + [img.width - crop_pad[0], img.height - crop_pad[1]]
     img.crop(crop_box).show()
